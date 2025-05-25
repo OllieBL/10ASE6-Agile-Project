55d5ed6506624d43f80a45b354f9f1a58fe7de2c
OllieBL <oliver.black4@education.nsw.gov.au>
2025-05-25 12:51:20 +1000

    board
    begun work on the board, it doesn't work properly yet, but should work soon. That is the next thing that needs to be completed for the project

---
+++ Code/board.py
@@ -0,0 +1,51 @@
+import pygame
+from sys import exit
+import combat_objects
+
+
+# the board class for combat to take place on. it just creates the board and stores the locations and id's of objects on the board for the combat functions to take place
+class CombatBoard:
+    def __init__(self, combat_objects, board_dimensions, screen):
+        self._combat_objects = combat_objects
+        self._board_dimensions = board_dimensions
+        self._board_tiles = []
+        self.screen = screen
+
+        self.create_board()
+        self.display_board()
+    
+    def create_board(self):
+        for i in range(self._board_dimensions[0]):
+            self._board_tiles.append([])
+            for j in range(self._board_dimensions[1]):
+                self._board_tiles[i].append(Tile(False))
+
+    def display_board(self):
+        while True:
+            for i in range(len(self._board_tiles)):
+                for j in range(len(self._board_tiles[i]), 2):
+                    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect((i*10+10), (j*10+10), 10, 10))
+                    pygame.draw.rect(screen, (200, 200, 200), pygame.Rect((i*10+10), (j*10+10), 10, 10))
+            pygame.display.flip()
+                    
+
+
+
+
+
+class Tile:
+    def __init__(self, combat_object):
+        self._combat_object = combat_object
+
+    def get_combat_object(self):
+        return self._combat_object
+    
+    def set_combat_object(self, combat_object):
+        self._combat_object = combat_object
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+
+board = CombatBoard([], [5, 5], screen)
--- Code/combat_objects.py
+++ Code/combat_objects.py
@@ -2,12 +2,13 @@ from abc import ABC, abstractmethod
 
 
 class CombatObject(ABC):
-    def __init__(self, health, speed, damage, inventory, object_type):
+    def __init__(self, health, speed, damage, inventory, combat_object_type, board_position):
         self._health = health
         self._speed = speed
         self._damage = damage
         self._inventory = inventory
-        self._type = object_type
+        self._combat_object_type = combat_object_type
+        self._board_position = board_position
 
     @abstractmethod
     def attack(self):
@@ -29,6 +30,12 @@ class CombatObject(ABC):
 
     def get_inventory(self):
         return self._inventory
+    
+    def get_combat_object_type(self):
+        return self._combat_object_type
+    
+    def get_board_position(self):
+        return self._board_position
 
 class Player(CombatObject):
     def __init__(self, health, speed, damage, inventory, object_type, spells):
