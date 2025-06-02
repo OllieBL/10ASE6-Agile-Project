831667f3d87702d3bd450cac413c8df60f4928a5
OllieBL <oliver.black4@education.nsw.gov.au>
2025-06-02 11:05:28 +1000

    added movement
    added movement, reformatted display_board function to add a load_images function to help with movement and updating the board. begun coding enemy ai. the next step is to combine the enemy ai with the movement so the enemy can chase the player

--- Code/board.py
+++ Code/board.py
@@ -10,16 +10,20 @@ class CombatBoard:
         self._combat_objects = combat_objects
         self._board_dimensions = board_dimensions
         self._board_tiles = []
+        self._image_list = []
         self.screen = screen
 
         self.create_board()
+        self.load_images()
         self.display_board()
     
     def create_board(self):
         for i in range(self._board_dimensions[0]):
             self._board_tiles.append([])
+
             for j in range(self._board_dimensions[1]):
                 validator = False
+
                 for x in self._combat_objects:
                     if x.get_board_position() == [i, j]:
                         self._board_tiles[i].append(Tile(x))
@@ -28,36 +32,76 @@ class CombatBoard:
                 if validator == False:
                     self._board_tiles[i].append(Tile(False))
 
-    def display_board(self):
-        image_list = []
+    def load_images(self):
+        self._image_list = []
+
         for i in range(len(self._board_tiles)):
-            image_list.append([])
+            self._image_list.append([])
+            
             for j in range(len(self._board_tiles[i])):
                 if self._board_tiles[i][j].get_combat_object() != False:
                     tile_image = pygame.image.load(self._board_tiles[i][j].get_combat_object().get_image()).convert()
                     tile_image = pygame.transform.scale(tile_image, (30, 30))
+
                 else:
                     tile_image = False
-                image_list[i].append(tile_image)
 
 
+                self._image_list[i].append(tile_image)
+
+    def display_board(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit()
+                test = self.update_board(event)
+
+                if test == True:
+                    self.load_images()
 
 
             for i in range(len(self._board_tiles)):
                 for j in range(len(self._board_tiles[i])):
-                    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect((i*35+10), (j*35+10), 30, 30))
-                    if image_list[i][j] != False:
-                        screen.blit(image_list[i][j], (i*35+10, j*35+10))
+                    pygame.draw.rect(self.screen, (255, 255, 255), pygame.Rect((i*35+10), (j*35+10), 30, 30))
+                    if self._image_list[i][j] != False:
+                        self.screen.blit(self._image_list[i][j], (i*35+10, j*35+10))
             
 
             pygame.display.flip()
-                    
+
+    def update_board(self, event):
+        if event.type == KEYDOWN:
+            current_position = [self._combat_objects[0].get_board_position()[0], self._combat_objects[0].get_board_position()[1]]
 
 
+            if event.key == K_LEFT and current_position[0] > 0:
+                self._combat_objects[0].set_board_position([current_position[0] - 1, current_position[1]])
+                new_position = [current_position[0] - 1, current_position[1]]
+                print('went left')
+
+            elif event.key == K_RIGHT and current_position[0] < (self._board_dimensions[0] - 1):
+                self._combat_objects[0].set_board_position([current_position[0] + 1, current_position[1]])
+                new_position = [current_position[0] + 1, current_position[1]]
+                print('went right')
+
+            elif event.key == K_UP and current_position[1] > 0:
+                self._combat_objects[0].set_board_position([current_position[0], current_position[1] - 1])
+                new_position = [current_position[0], current_position[1] - 1]
+                print('went up')
+
+            elif event.key == K_DOWN and current_position[1] < (self._board_dimensions[1] - 1):
+                self._combat_objects[0].set_board_position([current_position[0], current_position[1] + 1])
+                new_position = [current_position[0], current_position[1] + 1]
+                print('went right')
+
+            else:
+                new_position = current_position
+
+            
+            self._board_tiles[current_position[0]][current_position[1]] = Tile(False)
+            self._board_tiles[new_position[0]][new_position[1]] = Tile(self._combat_objects[0])
+            
+            return True
 
 
 
@@ -74,6 +118,6 @@ class Tile:
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
 
-board = CombatBoard([combat_objects.Player(0, 0, 0, 0, 0, [0, 0], 'Images and other files/test_image.png', 0), combat_objects.Player(0, 0, 0, 0, 0, [1, 1], 'Images and other files/test_image.png', 0)], [5, 5], screen)
+board = CombatBoard([combat_objects.Player(0, 0, 0, 0, 0, [0, 0], 'Images and other files/test_image.png', 0), combat_objects.Enemy(0, 0, 0, 0, 0, [10, 10], 'Images and other files/enemy_art.png')], [20, 20], screen)
--- Code/combat_objects.py
+++ Code/combat_objects.py
@@ -39,6 +39,24 @@ class CombatObject(ABC):
     
     def get_board_position(self):
         return self._board_position
+    
+    def set_image(self, image):
+        self._image = image
+
+    def set_speed(self, speed):
+        self._speed = speed
+
+    def set_damage(self, damage):
+        self._damage = damage
+
+    def set_inventory(self, inventory):
+        self._inventory = inventory
+    
+    def set_combat_object_type(self, combat_object_type):
+        self._combat_object_type = combat_object_type
+    
+    def set_board_position(self, board_position):
+        self._board_position = board_position
 
 class Player(CombatObject):
     def __init__(self, health, speed, damage, inventory, object_type, board_position, image, spells):
@@ -50,3 +68,70 @@ class Player(CombatObject):
 
     def get_image(self):
         return super().get_image()
+    
+    def get_speed(self):
+        return super().get_speed()
+    
+    def get_health(self):
+        return super().get_health()
+    
+    def get_damage(self):
+        return super().get_damage()
+    
+    def get_inventory(self):
+        return super().get_inventory()
+    
+    def get_board_position(self):
+        return super().get_board_position()
+    
+    def get_combat_object_type(self):
+        return super().get_combat_object_type()
+    
+    def set_speed(self, speed):
+        return super().set_speed(speed)
+    
+    def set_image(self, image):
+        return super().set_image(image)
+    
+    def set_damage(self, damage):
+        return super().set_damage(damage)
+    
+    def set_inventory(self, inventory):
+        return super().set_inventory(inventory)
+    
+    def set_board_position(self, board_position):
+        return super().set_board_position(board_position)
+    
+    def set_combat_object_type(self, combat_object_type):
+        return super().set_combat_object_type(combat_object_type)
+    
+class Enemy(CombatObject):
+    def __init__(self, health, speed, damage, inventory, combat_object_type, board_position, image):
+        super().__init__(health, speed, damage, inventory, combat_object_type, board_position, image)
+
+    def attack(self):
+        pass
+
+    def get_board_position(self):
+        return super().get_board_position()
+    
+    def set_board_position(self, board_position):
+        super().set_board_position(board_position)
+    
+    def get_image(self):
+        return super().get_image()
+    
+    def set_image(self, image):
+        super().set_image(image)
+
+    def decide_movement(self, player_board_position):
+        enemy_movement = []
+        distance_checker = []
+
+        
+        distance_checker.append(abs((self._board_position[0] - 1) - player_board_position[0]) ** 2 + abs(self._board_position[1] - player_board_position))
+        distance_checker.append(abs((self._board_position[0] + 1) - player_board_position[0]) ** 2 + abs(self._board_position[1] - player_board_position))
+        distance_checker.append(abs(self._board_position[0] - player_board_position[0]) ** 2 + abs((self._board_position[1] - 1) - player_board_position))
+        distance_checker.append(abs(self._board_position[0] - player_board_position[0]) ** 2 + abs((self._board_position[1] + 1)- player_board_position))
+
+        return min(distance_checker)
